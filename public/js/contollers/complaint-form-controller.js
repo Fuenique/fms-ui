16459999b8ccf2ae4c1b23d86abd09082ef8e469
fuenique <varunganji@gmail.com>
2018-03-31 02:57:00 +0530

    Refactor: add extra fields to new complaint

--- public/js/contollers/complaint-form-controller.js
+++ public/js/contollers/complaint-form-controller.js
@@ -17,24 +17,19 @@
             console.log($scope.formData);
             $scope.submitComplaint = function (files) {
                 console.log(files);
-                // for(var index = 0; index < files.length; index++){
-                //     payload.append('photos', files[index]);
-                // }
-                // payload.append('data', $scope.formData);
-                // console.log(payload);
                 if (files && files.length) {
                     Upload.upload({
                         url: "http://localhost:3000/public/complaints/newComplaint",
                         data: {
                             files: files,
-
+                            type: $scope.formData.type,
+                            affectedArea: $scope.formData.affectLength,
+                            affectDirection: $scope.formData.affectDirection,
                             description: $scope.formData.description,
                             severity: $scope.formData.severity,
                             area: $scope.formData.location,
                             pincode: $scope.formData.pincode,
                             geometry:  $scope.coordinates,
-                            
-
                         },
                         method: "POST",
                         headers: {
